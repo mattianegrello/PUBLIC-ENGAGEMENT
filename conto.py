7c3a52be0b40005349bdbe4648ed84fa2a488ed8
mattianegrello <132370129+mattianegrello@users.noreply.github.com>
2023-05-02 18:01:21 +0100

    Update conto.py

--- conto.py
+++ conto.py
@@ -2,8 +2,8 @@
 import numpy as np
 import streamlit as st
 
-lordo = st.number_input(label='Inserisci Lordo (in Euri) = ')
-IVA = st.number_input('Inserisci IVA (%) = ')
+lordo = st.number_input(label='Inserisci Lordo (in Euri)')
+IVA = st.number_input('Inserisci IVA (%)')
 netto = lordo/(1 + IVA/100.0)
 
 st.write()
