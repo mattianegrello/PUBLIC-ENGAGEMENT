59dec4091bc4a931cc850eac60a0e4f4d339631b
mattianegrello <132370129+mattianegrello@users.noreply.github.com>
2023-05-02 16:35:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,8 @@
 
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
 import time
 
 st.write('Hello')
