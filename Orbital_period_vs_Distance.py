eb855ef50502d69a5537907ba112813bb8fba2a4
mattianegrello <132370129+mattianegrello@users.noreply.github.com>
2023-05-12 17:34:19 +0100

    Update Orbital_period_vs_Distance.py

--- Orbital_period_vs_Distance.py
+++ Orbital_period_vs_Distance.py
@@ -737,7 +737,8 @@ with st.expander(label_expander):
     
     st.markdown('Immagine that a distant star has two planets orbiting around it, as illustrated in the picture below. **Planet 1** is located very close to the star while **Planet 2** is far away from the star. Which of the two planets is more likely to transit in front of the star while you are observing that system and why? Select the correct answer below.')
 
-    st.image('images/Planet_1_and_Planet_2.png')
+    image = plt.imread('images/Planet_1_and_Planet_2.png.png')
+    st.image(image)
     st.write('')
 
     answer_1 = 'Planet 1 is more likely to be seen transiting in front of the star'
